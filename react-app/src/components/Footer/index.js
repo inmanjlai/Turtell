b2e6f83056c8cdd054679aa2224f8ef51b314425
Ismail Manjlai <inmanjlai@hotmail.com>
2023-05-18 16:14:42 -0400

    deployed to heroku

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -3,8 +3,8 @@ import './footer.css'
 const Footer = () => {
     return (
         <div className='footer-links'>
-            <a href="https://inmanjlai.dev/">Ismail N Manjlai</a>
-            <a href="https://github.com/ocahsa">GitHub</a>
+            <a href="https://inmanjlai.github.io/">Ismail N Manjlai</a>
+            <a href="https://github.com/inmanjlai">GitHub</a>
             <a href="https://www.linkedin.com/in/inmanjlai/">LinkedIn</a>
         </div>
     )
